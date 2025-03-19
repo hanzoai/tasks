temporalio/temporal

Forward priority to matching (#7134)
## What changed?
<!-- Describe what has changed in this PR -->

Stores priorities from commands in MS, extracts it in task processing
when sending to matching.

- [x] StartWorkflowExecutionRequest
- [x] StartChildWorkflowExecutionInitiatedEventAttributes
- [x] ActivityTaskScheduledEventAttributes
- [x] WorkflowExecutionStartedEventAttributes
- [x] PollActivityTaskQueueResponse
- [x] SignalWithStartWorkflowExecutionRequest
- [x] RecordActivityTaskStartedResponse
- [x] ScheduleActivityTaskCommandAttributes
- [x] DescribeWorkflowExecution
- [x] NewWorkflowExecutionInfo
- [x] GetPendingActivityInfo

NOTE: haven't added any frontend validation yet.

## Why?
<!-- Tell your future self why have you made these changes -->

## How did you test it?
<!-- How have you verified this change? Tested locally? Added a unit
test? Checked in staging env? -->

Wrote unit test for merging priorities.

Tested a few scenarios like Start Workflow and Activity by hand and
checked the priority makes it to Matching.

Didn't write any functional tests yet, I suspect this will be covered by
future priority functional tests.

## Potential risks
<!-- Assuming the worst case, what can be broken when deploying this
change to production? -->

## Documentation
<!-- Have you made sure this change doesn't falsify anything currently
stated in `docs/`? If significant
new behavior is added, have you described that in `docs/`? -->

## Is hotfix candidate?
<!-- Is this PR a hotfix candidate or does it require a notification to
be sent to the broader community? (Yes/No) -->

---------

Co-authored-by: David Reiss <dnr@dnr.im>

common/priorities/priority_util.go
@@ -1,0 +1,46 @@
+// The MIT License
+//
+// Copyright (c) 2025 Temporal Technologies Inc.  All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package priorities
+
+import (
+	"cmp"
+
+	commonpb "go.temporal.io/api/common/v1"
+)
+
+func Merge(
+	base *commonpb.Priority,
+	override *commonpb.Priority,
+) *commonpb.Priority {
+	if base == nil || override == nil {
+		return cmp.Or(base, override)
+	}
+
+	return &commonpb.Priority{
+		PriorityKey:       cmp.Or(override.PriorityKey, base.PriorityKey),
+		FairnessKey:       cmp.Or(override.FairnessKey, base.FairnessKey),
+		FairnessWeight:    cmp.Or(override.FairnessWeight, base.FairnessWeight),
+		FairnessRateLimit: cmp.Or(override.FairnessRateLimit, base.FairnessRateLimit),
+		OrderingKey:       cmp.Or(override.OrderingKey, base.OrderingKey),
+	}
+}

hanzoai/tasks

common/priorities/priority_util.go
@@ -1,0 +1,42 @@
+// The MIT License
+//
+// Copyright (c) 2025 Temporal Technologies Inc.  All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+package priorities
+
+import (
+	"cmp"
+
+	commonpb "go.temporal.io/api/common/v1"
+)
+
+func Merge(
+	base *commonpb.Priority,
+	override *commonpb.Priority,
+) *commonpb.Priority {
+	if base == nil || override == nil {
+		return cmp.Or(override, base)
+	}
+
+	return &commonpb.Priority{
+		PriorityKey: cmp.Or(override.PriorityKey, base.PriorityKey),
+	}
+}