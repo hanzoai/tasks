temporalio/temporal

STAMP framework

common/testing/stamp/mdl_set.go
@@ -1,0 +1,205 @@
+package stamp
+
+import (
+	"fmt"
+	"reflect"
+	"regexp"
+	"slices"
+	"strings"
+)
+
+var (
+	scopeTypePattern   = regexp.MustCompile(`Scope\[(.*)]`)
+	embeddableMdlTypes = []reflect.Type{
+		reflect.TypeOf(&Model[Root]{}),
+		reflect.TypeOf(&Scope[Root]{}),
+		reflect.TypeFor[modelWrapper](),
+		reflect.TypeFor[Generator[any]](),
+	}
+)
+
+type (
+	ModelSet struct {
+		typeIdx             map[modelType]struct{}
+		typeByQualifiedName map[string]modelType
+		childTypes          map[modelType][]modelType
+		parentType          map[modelType]modelType
+		propMetadataIdx     map[modelType][]propMetadata
+		callbackFuncIdx     map[modelType]map[string]func(modelWrapper, routableAction) func(reflect.Value)
+	}
+	registerModel interface {
+		modelWrapper
+	}
+)
+
+func NewModelSet() *ModelSet {
+	return &ModelSet{
+		typeIdx:             make(map[modelType]struct{}),
+		typeByQualifiedName: map[string]modelType{rootTypeName: rootType},
+		childTypes:          make(map[modelType][]modelType),
+		parentType:          make(map[modelType]modelType),
+		propMetadataIdx:     make(map[modelType][]propMetadata),
+		callbackFuncIdx:     make(map[modelType]map[string]func(modelWrapper, routableAction) func(reflect.Value)),
+	}
+}
+
+// TODO: check that struct has no fields at all (we don't want any state there)
+// TODO: type check parent matches model somehow?
+// TODO: fail when 2 params of a handler have the same type
+// TODO: check if handler contains unexpected parameter types (ie from outside of this context)
+func RegisterModel[M registerModel](set *ModelSet) {
+	ptrType := reflect.TypeFor[M]()
+	elemType := ptrType.Elem()
+	mdlType := modelType{ptrType: ptrType, structType: elemType, name: elemType.Name()}
+
+	if _, ok := set.typeIdx[mdlType]; ok {
+		panic(fmt.Sprintf("%q already registered", mdlType.name))
+	}
+	set.typeIdx[mdlType] = struct{}{}
+	set.typeByQualifiedName[qualifiedTypeName(mdlType.structType)] = mdlType
+
+	for i := 0; i < mdlType.structType.NumField(); i++ {
+		field := mdlType.structType.Field(i)
+		fieldTypeName := field.Type.String()
+		parentMatch := scopeTypePattern.FindStringSubmatch(fieldTypeName)
+		if len(parentMatch) == 2 {
+			scopeFieldMdlTypeName := strings.TrimPrefix(parentMatch[1], "*")
+			scopeFieldMdlType, ok := set.typeByQualifiedName[scopeFieldMdlTypeName]
+			if !ok {
+				panic(fmt.Sprintf("scope %q from model %q must be a registered model", scopeFieldMdlTypeName, mdlType.name))
+			}
+			set.childTypes[scopeFieldMdlType] = append(set.childTypes[scopeFieldMdlType], mdlType)
+			set.parentType[mdlType] = scopeFieldMdlType
+		}
+	}
+
+	// register model handlers
+	// TODO: scan unexported methods, too, in case user accidentally made one private
+	//mdlInst := reflect.New(mdlType.structType).Interface().(modelWrapper)
+	//mdlInstVal := reflect.ValueOf(mdlInst)
+	for i := 0; i < ptrType.NumMethod(); i++ {
+		method := ptrType.Method(i)
+		methodType := method.Type
+		if isFromEmbedded(method) {
+			continue
+		}
+
+		switch {
+		// ignore model getters
+		case methodType.NumIn() == 1 && methodType.NumOut() == 1 && strings.HasPrefix(method.Name, "Get"):
+			// TODO: check that return type is a model
+			continue
+
+		// index action handlers
+		case methodType.NumIn() == 2 && methodType.NumOut() == 1 && methodType.Out(0).Kind() == reflect.Func:
+			if _, ok := set.callbackFuncIdx[mdlType]; !ok {
+				set.callbackFuncIdx[mdlType] = make(map[string]func(modelWrapper, routableAction) func(reflect.Value))
+			}
+			inputType := methodType.In(1)
+			set.callbackFuncIdx[mdlType][inputType.String()] = func(mw modelWrapper, action routableAction) func(reflect.Value) {
+				ret := method.Func.Call([]reflect.Value{reflect.ValueOf(mw), reflect.ValueOf(action)})
+				return func(v reflect.Value) { ret[0].Call([]reflect.Value{v}) }
+			}
+
+		default:
+			panic(fmt.Sprintf("method %q on %q is not a getter or an action handler", method.Name, mdlType.name))
+		}
+	}
+
+	// index model properties
+	// TODO: parse doc tag and add it to the property
+	for i := 0; i < elemType.NumField(); i++ {
+		field := elemType.Field(i)
+		switch {
+		case field.Type.Implements(propCopyType):
+			pm := propMetadata{name: field.Name, t: field.Type}
+			set.propMetadataIdx[mdlType] = append(set.propMetadataIdx[mdlType], pm)
+		}
+	}
+}
+
+func (s ModelSet) newModel(
+	env modelEnv,
+	id ID,
+	mdlType modelType,
+	scope modelWrapper,
+) modelWrapper {
+	mdl := &internalModel{
+		mdlEnv: env,
+		typeOf: mdlType,
+	}
+	mdl.updateIdentity(scope.getKey(), id)
+
+	v := reflect.New(mdlType.structType)
+	mw := v.Interface().(modelWrapper)
+	mw.setModel(mdl)
+	mw.setScope(scope)
+
+	// apply property metadata
+	for _, pm := range s.propMetadataIdx[mdlType] {
+		field := v.Elem().FieldByName(pm.getName())
+		curProp := field.Interface().(propCopier)
+		newProp := curProp.createFromMetadata(pm)
+		field.Set(reflect.ValueOf(newProp))
+	}
+
+	return mw
+}
+
+func (s ModelSet) consume(mw modelWrapper, action routableAction) func(reflect.Value) {
+	mdlCallbacks := s.callbackFuncIdx[mw.getType()]
+	if mdlCallbacks == nil {
+		return nil
+	}
+	actionTypeStr := fmt.Sprintf("%T", action)
+	actionCallback := mdlCallbacks[actionTypeStr]
+	if actionCallback == nil {
+		return nil
+	}
+	return actionCallback(mw, action)
+}
+
+func (s ModelSet) validate(a any) {
+	if reflect.TypeOf(a).Kind() != reflect.Struct {
+		panic(fmt.Sprintf("expected struct, got %T", a))
+	}
+	err := validator.Struct(a)
+	if err != nil {
+		// TODO: use logger
+		panic(fmt.Sprintf("%T failed validation: %v", a, err))
+	}
+}
+
+func (s ModelSet) childTypesOf(m modelWrapper) []modelType {
+	return s.childTypes[m.getType()]
+}
+
+func (s ModelSet) pathTo(ty reflect.Type) []modelType {
+	typeName := qualifiedTypeName(ty)
+	curType, ok := s.typeByQualifiedName[typeName]
+	if !ok {
+		panic(fmt.Sprintf("type %q is not a registered model", ty))
+	}
+
+	var res []modelType
+	for curType != rootType {
+		res = append(res, curType)
+		curType = s.parentType[curType]
+	}
+	slices.Reverse(res)
+	return res
+}
+
+func isFromEmbedded(method reflect.Method) bool {
+	for _, typ := range embeddableMdlTypes {
+		if _, ok := typ.MethodByName(method.Name); ok {
+			return true
+		}
+		if typ.Kind() == reflect.Ptr {
+			if _, ok := typ.Elem().MethodByName(method.Name); ok {
+				return true
+			}
+		}
+	}
+	return false
+}

tests/acceptance/model/workflow_execution.go
@@ -1,0 +1,98 @@
+package model
+
+import (
+	"cmp"
+
+	"github.com/pborman/uuid"
+	commonpb "go.temporal.io/api/common/v1"
+	enumspb "go.temporal.io/api/enums/v1"
+	"go.temporal.io/api/workflowservice/v1"
+	"go.temporal.io/server/common/testing/stamp"
+)
+
+type (
+	WorkflowExecution struct {
+		stamp.Model[*WorkflowExecution]
+		stamp.Scope[*Workflow]
+
+		RequestID string
+		Status    enumspb.WorkflowExecutionStatus
+
+		IsRunning  stamp.Marker
+		IsComplete stamp.Marker
+	}
+)
+
+func (w *WorkflowExecution) GetNamespace() *Namespace {
+	return w.GetScope().GetNamespace()
+}
+
+func (w *WorkflowExecution) OnWorkflowStart(
+	in IncomingAction[*workflowservice.StartWorkflowExecutionRequest],
+) func(OutgoingAction[*workflowservice.StartWorkflowExecutionResponse]) {
+	switch {
+	case in.Request.WorkflowIdReusePolicy == enumspb.WORKFLOW_ID_REUSE_POLICY_TERMINATE_IF_RUNNING &&
+		in.Request.WorkflowIdConflictPolicy != enumspb.WORKFLOW_ID_CONFLICT_POLICY_UNSPECIFIED:
+		in.AddValidationError("Invalid WorkflowIDReusePolicy")
+	}
+
+	var newStart bool
+	in.Request.RequestId = cmp.Or(in.Request.RequestId, uuid.New())
+	if w.RequestID != in.Request.RequestId {
+		w.RequestID = in.Request.RequestId
+		w.Status = enumspb.WORKFLOW_EXECUTION_STATUS_RUNNING
+		newStart = true
+	}
+
+	return func(out OutgoingAction[*workflowservice.StartWorkflowExecutionResponse]) {
+		switch {
+		case out.ResponseErr == nil:
+			if newStart {
+				w.SetID(out.Response.RunId)
+			}
+		}
+	}
+}
+
+func (w *WorkflowExecution) OnDescribeWorkflowExecution(
+	_ IncomingAction[*workflowservice.DescribeWorkflowExecutionRequest],
+) func(out OutgoingAction[*workflowservice.DescribeWorkflowExecutionResponse]) {
+	return func(out OutgoingAction[*workflowservice.DescribeWorkflowExecutionResponse]) {
+		// TODO
+	}
+}
+
+func (w *WorkflowExecution) OnTerminateWorkflowExecution(
+	_ IncomingAction[*workflowservice.TerminateWorkflowExecutionRequest],
+) func(out OutgoingAction[*workflowservice.TerminateWorkflowExecutionResponse]) {
+	return func(out OutgoingAction[*workflowservice.TerminateWorkflowExecutionResponse]) {
+		switch {
+		case out.ResponseErr == nil:
+			w.Status = enumspb.WORKFLOW_EXECUTION_STATUS_TERMINATED
+		}
+	}
+}
+
+func (w *WorkflowExecution) OnRespondWorkflowTaskCompleted(
+	in IncomingAction[*workflowservice.RespondWorkflowTaskCompletedRequest],
+) func(out OutgoingAction[*workflowservice.RespondWorkflowTaskCompletedResponse]) {
+	for _, commands := range in.Request.Commands {
+		switch commands.CommandType {
+		case enumspb.COMMAND_TYPE_COMPLETE_WORKFLOW_EXECUTION:
+			w.Status = enumspb.WORKFLOW_EXECUTION_STATUS_COMPLETED
+		}
+	}
+	return func(out OutgoingAction[*workflowservice.RespondWorkflowTaskCompletedResponse]) {}
+}
+
+func (w *WorkflowExecution) Verify() {
+	w.IsRunning.Set(w.Status == enumspb.WORKFLOW_EXECUTION_STATUS_RUNNING)
+	w.IsComplete.Set(w.Status == enumspb.WORKFLOW_EXECUTION_STATUS_COMPLETED)
+}
+
+func (w *WorkflowExecution) Next(ctx stamp.GenContext) *commonpb.WorkflowExecution {
+	return &commonpb.WorkflowExecution{
+		WorkflowId: string(w.GetScope().GetID()),
+		RunId:      "", // TODO: use current ID
+	}
+}

tests/acceptance/worker_versioning_test.go
@@ -1,0 +1,66 @@
+package acceptance
+
+import (
+	"testing"
+
+	enumspb "go.temporal.io/api/enums/v1"
+	"go.temporal.io/server/common/dynamicconfig"
+	. "go.temporal.io/server/common/testing/stamp"
+	"go.temporal.io/server/tests/acceptance/model"
+	. "go.temporal.io/server/tests/acceptance/testenv"
+	. "go.temporal.io/server/tests/acceptance/testenv/action"
+)
+
+func TestWorkerVersioning(t *testing.T) {
+	ts := NewTestSuite(t)
+
+	t.Run("Start child workflow", func(t *testing.T) {
+		ts.Run(t, func(s *Scenario) {
+			c, ns, tq, usr, wkr := ts.NewWorkflowStack(s)
+			c.SetNamespaceDynamicConfig(ns, dynamicconfig.EnableDeploymentVersions, true)
+			dpl := Act(c, CreateWorkerDeployment{Namespace: ns})
+			dvrA := Act(c, CreateWorkerDeploymentVersion{Deployment: dpl, Name: GenJust[ID]("A")})
+			dvrB := Act(c, CreateWorkerDeploymentVersion{Deployment: dpl, Name: GenJust[ID]("B")})
+
+			// Make version "A" current
+			wftA := ActAsync(wkr, PollWorkflowTaskQueue{TaskQueue: tq, Version: dvrA})
+			s.Await(&dvrA.IsCurrent, OnRetry(func() {
+				_, _ = ActTry(c, SetWorkerDeploymentCurrentVersion{Version: dvrA})
+			}))
+
+			// Start workflow
+			Act(usr, StartWorkflowExecution{TaskQueue: tq})
+			wftA.Await()
+
+			// Make version "B" current
+			wftB := ActAsync(wkr, PollWorkflowTaskQueue{TaskQueue: tq, Version: dvrB})
+			s.Await(&dvrB.IsCurrent, OnRetry(func() {
+				_, _ = ActTry(c, SetWorkerDeploymentCurrentVersion{Version: dvrB})
+			}))
+			wftB.Cancel()
+
+			// Start child workflow
+			childTaskQueue := GenChoice[*model.TaskQueue]("child task queue",
+				tq,                                     // TODO: add labels
+				Act(c, CreateTaskQueue{Namespace: ns}), // TODO: do lazy eval
+			).Next(s)
+			wft := Act(wkr, RespondWorkflowTaskCompleted{
+				WorkflowTask:       wftA.Await(),
+				Commands:           GenList(StartChildWorkflowExecutionCommand{TaskQueue: childTaskQueue}),
+				Version:            dvrA,
+				VersioningBehavior: ANY_VERSIONING_BEHAVIOR,
+			})
+
+			// Complete child workflow
+			pullChildWorkflowVersion := dvrB
+			if childTaskQueue == tq && wft.Response.VersioningBehavior == enumspb.VERSIONING_BEHAVIOR_PINNED {
+				pullChildWorkflowVersion = dvrA
+			}
+			cwft := Act(wkr, PollWorkflowTaskQueue{TaskQueue: childTaskQueue, Version: pullChildWorkflowVersion})
+			Act(wkr, RespondWorkflowTaskCompleted{
+				WorkflowTask: cwft,
+				Commands:     GenList(CompleteWorkflowExecutionCommand{}),
+			})
+		})
+	})
+}

hanzoai/tasks

common/testing/stamp/mdl_set.go
@@ -1,0 +1,205 @@
+package stamp
+
+import (
+	"fmt"
+	"reflect"
+	"regexp"
+	"slices"
+	"strings"
+)
+
+var (
+	scopeTypePattern   = regexp.MustCompile(`Scope\[(.*)]`)
+	embeddableMdlTypes = []reflect.Type{
+		reflect.TypeOf(&Model[Root]{}),
+		reflect.TypeOf(&Scope[Root]{}),
+		reflect.TypeFor[modelWrapper](),
+		reflect.TypeFor[Arbitrary[any]](),
+	}
+)
+
+type (
+	ModelSet struct {
+		typeIdx             map[modelType]struct{}
+		typeByQualifiedName map[string]modelType
+		childTypes          map[modelType][]modelType
+		parentType          map[modelType]modelType
+		propMetadataIdx     map[modelType][]propMetadata
+		callbackFuncIdx     map[modelType]map[string]func(modelWrapper, routableAction) func(reflect.Value)
+	}
+	registerModel interface {
+		modelWrapper
+	}
+)
+
+func NewModelSet() *ModelSet {
+	return &ModelSet{
+		typeIdx:             make(map[modelType]struct{}),
+		typeByQualifiedName: map[string]modelType{rootTypeName: rootType},
+		childTypes:          make(map[modelType][]modelType),
+		parentType:          make(map[modelType]modelType),
+		propMetadataIdx:     make(map[modelType][]propMetadata),
+		callbackFuncIdx:     make(map[modelType]map[string]func(modelWrapper, routableAction) func(reflect.Value)),
+	}
+}
+
+// TODO: check that struct has no fields at all (we don't want any state there)
+// TODO: type check parent matches model somehow?
+// TODO: fail when 2 params of a handler have the same type
+// TODO: check if handler contains unexpected parameter types (ie from outside of this context)
+func RegisterModel[M registerModel](set *ModelSet) {
+	ptrType := reflect.TypeFor[M]()
+	elemType := ptrType.Elem()
+	mdlType := modelType{ptrType: ptrType, structType: elemType, name: elemType.Name()}
+
+	if _, ok := set.typeIdx[mdlType]; ok {
+		panic(fmt.Sprintf("%q already registered", mdlType.name))
+	}
+	set.typeIdx[mdlType] = struct{}{}
+	set.typeByQualifiedName[qualifiedTypeName(mdlType.structType)] = mdlType
+
+	for i := 0; i < mdlType.structType.NumField(); i++ {
+		field := mdlType.structType.Field(i)
+		fieldTypeName := field.Type.String()
+		parentMatch := scopeTypePattern.FindStringSubmatch(fieldTypeName)
+		if len(parentMatch) == 2 {
+			scopeFieldMdlTypeName := strings.TrimPrefix(parentMatch[1], "*")
+			scopeFieldMdlType, ok := set.typeByQualifiedName[scopeFieldMdlTypeName]
+			if !ok {
+				panic(fmt.Sprintf("scope %q from model %q must be a registered model", scopeFieldMdlTypeName, mdlType.name))
+			}
+			set.childTypes[scopeFieldMdlType] = append(set.childTypes[scopeFieldMdlType], mdlType)
+			set.parentType[mdlType] = scopeFieldMdlType
+		}
+	}
+
+	// register model handlers
+	// TODO: scan unexported methods, too, in case user accidentally made one private
+	//mdlInst := reflect.New(mdlType.structType).Interface().(modelWrapper)
+	//mdlInstVal := reflect.ValueOf(mdlInst)
+	for i := 0; i < ptrType.NumMethod(); i++ {
+		method := ptrType.Method(i)
+		methodType := method.Type
+		if isFromEmbedded(method) {
+			continue
+		}
+
+		switch {
+		// ignore model getters
+		case methodType.NumIn() == 1 && methodType.NumOut() == 1 && strings.HasPrefix(method.Name, "Get"):
+			// TODO: check that return type is a model
+			continue
+
+		// index action handlers
+		case methodType.NumIn() == 2 && methodType.NumOut() == 1 && methodType.Out(0).Kind() == reflect.Func:
+			if _, ok := set.callbackFuncIdx[mdlType]; !ok {
+				set.callbackFuncIdx[mdlType] = make(map[string]func(modelWrapper, routableAction) func(reflect.Value))
+			}
+			inputType := methodType.In(1)
+			set.callbackFuncIdx[mdlType][inputType.String()] = func(mw modelWrapper, action routableAction) func(reflect.Value) {
+				ret := method.Func.Call([]reflect.Value{reflect.ValueOf(mw), reflect.ValueOf(action)})
+				return func(v reflect.Value) { ret[0].Call([]reflect.Value{v}) }
+			}
+
+		default:
+			panic(fmt.Sprintf("method %q on %q is not a getter or an action handler", method.Name, mdlType.name))
+		}
+	}
+
+	// index model properties
+	// TODO: parse doc tag and add it to the property
+	for i := 0; i < elemType.NumField(); i++ {
+		field := elemType.Field(i)
+		switch {
+		case field.Type.Implements(propClonerType):
+			pm := propMetadata{name: field.Name, t: field.Type}
+			set.propMetadataIdx[mdlType] = append(set.propMetadataIdx[mdlType], pm)
+		}
+	}
+}
+
+func (s ModelSet) newModel(
+	env modelEnv,
+	id ID,
+	mdlType modelType,
+	scope modelWrapper,
+) modelWrapper {
+	mdl := &internalModel{
+		mdlEnv: env,
+		typeOf: mdlType,
+	}
+	mdl.updateIdentity(scope.getKey(), id)
+
+	v := reflect.New(mdlType.structType)
+	mw := v.Interface().(modelWrapper)
+	mw.setModel(mdl)
+	mw.setScope(scope)
+
+	// apply property metadata
+	for _, pm := range s.propMetadataIdx[mdlType] {
+		field := v.Elem().FieldByName(pm.getName())
+		curProp := field.Interface().(propCloner)
+		newProp := curProp.createFromMetadata(pm)
+		field.Set(reflect.ValueOf(newProp))
+	}
+
+	return mw
+}
+
+func (s ModelSet) consume(mw modelWrapper, action routableAction) func(reflect.Value) {
+	mdlCallbacks := s.callbackFuncIdx[mw.getType()]
+	if mdlCallbacks == nil {
+		return nil
+	}
+	actionTypeStr := fmt.Sprintf("%T", action)
+	actionCallback := mdlCallbacks[actionTypeStr]
+	if actionCallback == nil {
+		return nil
+	}
+	return actionCallback(mw, action)
+}
+
+func (s ModelSet) validate(a any) {
+	if reflect.TypeOf(a).Kind() != reflect.Struct {
+		panic(fmt.Sprintf("expected struct, got %T", a))
+	}
+	err := validator.Struct(a)
+	if err != nil {
+		// TODO: use logger
+		panic(fmt.Sprintf("%T failed validation: %v", a, err))
+	}
+}
+
+func (s ModelSet) childTypesOf(m modelWrapper) []modelType {
+	return s.childTypes[m.getType()]
+}
+
+func (s ModelSet) pathTo(ty reflect.Type) []modelType {
+	typeName := qualifiedTypeName(ty)
+	curType, ok := s.typeByQualifiedName[typeName]
+	if !ok {
+		panic(fmt.Sprintf("type %q is not a registered model", ty))
+	}
+
+	var res []modelType
+	for curType != rootType {
+		res = append(res, curType)
+		curType = s.parentType[curType]
+	}
+	slices.Reverse(res)
+	return res
+}
+
+func isFromEmbedded(method reflect.Method) bool {
+	for _, typ := range embeddableMdlTypes {
+		if _, ok := typ.MethodByName(method.Name); ok {
+			return true
+		}
+		if typ.Kind() == reflect.Ptr {
+			if _, ok := typ.Elem().MethodByName(method.Name); ok {
+				return true
+			}
+		}
+	}
+	return false
+}

tests/acceptance/model/workflow_execution.go
@@ -1,0 +1,108 @@
+package model
+
+import (
+	"cmp"
+
+	"github.com/pborman/uuid"
+	commonpb "go.temporal.io/api/common/v1"
+	enumspb "go.temporal.io/api/enums/v1"
+	"go.temporal.io/api/workflowservice/v1"
+	"go.temporal.io/server/common/testing/stamp"
+)
+
+type (
+	WorkflowExecution struct {
+		stamp.Model[*WorkflowExecution]
+		stamp.Scope[*Workflow]
+
+		Request   *workflowservice.StartWorkflowExecutionRequest
+		RequestID string
+		Status    enumspb.WorkflowExecutionStatus
+
+		IsRunning  stamp.Marker
+		IsComplete stamp.Marker
+	}
+)
+
+func (w *WorkflowExecution) GetNamespace() *Namespace {
+	return w.GetScope().GetNamespace()
+}
+
+func (w *WorkflowExecution) OnStartWorkflowExecution(
+	in IncomingAction[*workflowservice.StartWorkflowExecutionRequest],
+) func(OutgoingAction[*workflowservice.StartWorkflowExecutionResponse]) {
+	w.Request = in.Request
+
+	switch {
+	case in.Request.WorkflowIdReusePolicy == enumspb.WORKFLOW_ID_REUSE_POLICY_TERMINATE_IF_RUNNING &&
+		in.Request.WorkflowIdConflictPolicy != enumspb.WORKFLOW_ID_CONFLICT_POLICY_UNSPECIFIED:
+		in.AddValidationError("Invalid WorkflowIDReusePolicy")
+	}
+
+	var newStart bool
+	in.Request.RequestId = cmp.Or(in.Request.RequestId, uuid.New())
+	if w.RequestID != in.Request.RequestId {
+		w.RequestID = in.Request.RequestId
+		w.Status = enumspb.WORKFLOW_EXECUTION_STATUS_RUNNING
+		newStart = true
+	}
+
+	return func(out OutgoingAction[*workflowservice.StartWorkflowExecutionResponse]) {
+		switch {
+		case out.ResponseErr == nil:
+			if newStart {
+				w.SetID(out.Response.RunId)
+			}
+		}
+	}
+}
+
+func (w *WorkflowExecution) OnExecuteMultiOperationRequest(
+	_ IncomingAction[*workflowservice.ExecuteMultiOperationRequest],
+) func(OutgoingAction[*workflowservice.ExecuteMultiOperationResponse]) {
+	// TODO
+	return func(o OutgoingAction[*workflowservice.ExecuteMultiOperationResponse]) {}
+}
+
+func (w *WorkflowExecution) OnDescribeWorkflowExecution(
+	_ IncomingAction[*workflowservice.DescribeWorkflowExecutionRequest],
+) func(out OutgoingAction[*workflowservice.DescribeWorkflowExecutionResponse]) {
+	return func(out OutgoingAction[*workflowservice.DescribeWorkflowExecutionResponse]) {
+		// TODO
+	}
+}
+
+func (w *WorkflowExecution) OnTerminateWorkflowExecution(
+	_ IncomingAction[*workflowservice.TerminateWorkflowExecutionRequest],
+) func(out OutgoingAction[*workflowservice.TerminateWorkflowExecutionResponse]) {
+	return func(out OutgoingAction[*workflowservice.TerminateWorkflowExecutionResponse]) {
+		switch {
+		case out.ResponseErr == nil:
+			w.Status = enumspb.WORKFLOW_EXECUTION_STATUS_TERMINATED
+		}
+	}
+}
+
+func (w *WorkflowExecution) OnRespondWorkflowTaskCompleted(
+	in IncomingAction[*workflowservice.RespondWorkflowTaskCompletedRequest],
+) func(out OutgoingAction[*workflowservice.RespondWorkflowTaskCompletedResponse]) {
+	for _, commands := range in.Request.Commands {
+		switch commands.CommandType {
+		case enumspb.COMMAND_TYPE_COMPLETE_WORKFLOW_EXECUTION:
+			w.Status = enumspb.WORKFLOW_EXECUTION_STATUS_COMPLETED
+		}
+	}
+	return func(out OutgoingAction[*workflowservice.RespondWorkflowTaskCompletedResponse]) {}
+}
+
+func (w *WorkflowExecution) Verify() {
+	w.IsRunning.Set(w.Status == enumspb.WORKFLOW_EXECUTION_STATUS_RUNNING)
+	w.IsComplete.Set(w.Status == enumspb.WORKFLOW_EXECUTION_STATUS_COMPLETED)
+}
+
+func (w *WorkflowExecution) Next(ctx stamp.GenContext) *commonpb.WorkflowExecution {
+	return &commonpb.WorkflowExecution{
+		WorkflowId: string(w.GetScope().GetID()),
+		RunId:      "", // TODO: use current ID
+	}
+}

tests/acceptance/worker_versioning_test.go
@@ -1,0 +1,67 @@
+package acceptance
+
+import (
+	"testing"
+
+	enumspb "go.temporal.io/api/enums/v1"
+	"go.temporal.io/server/common/dynamicconfig"
+	. "go.temporal.io/server/common/testing/stamp"
+	"go.temporal.io/server/tests/acceptance/model"
+	. "go.temporal.io/server/tests/acceptance/testenv"
+	. "go.temporal.io/server/tests/acceptance/testenv/action"
+)
+
+func TestWorkerVersioning(t *testing.T) {
+	ts := NewTestSuite(t)
+
+	t.Run("Start child workflow", func(t *testing.T) {
+		ts.Run(t, func(s *Scenario) {
+			c, ns, tq, usr, wkr := ts.NewWorkflowStack(s)
+			Act(c, SetNamespaceConfig[bool]{NS: ns, Key: dynamicconfig.EnableDeploymentVersions, Val: true}) // TODO
+
+			dpl := Act(c, CreateWorkerDeployment{Namespace: ns})
+			dvrA := Act(c, CreateWorkerDeploymentVersion{Deployment: dpl, Name: GenJust[ID]("A")})
+			dvrB := Act(c, CreateWorkerDeploymentVersion{Deployment: dpl, Name: GenJust[ID]("B")})
+
+			// Make version "A" current
+			wftA := ActAsync(wkr, PollWorkflowTaskQueue{TaskQueue: tq, Version: dvrA})
+			s.Await(&dvrA.IsCurrent, OnRetry(func() {
+				_, _ = TryAct(c, SetWorkerDeploymentCurrentVersion{Version: dvrA})
+			}))
+
+			// Start workflow
+			Act(usr, StartWorkflowExecution{TaskQueue: tq})
+			wftA.Await()
+
+			// Make version "B" current
+			wftB := ActAsync(wkr, PollWorkflowTaskQueue{TaskQueue: tq, Version: dvrB})
+			s.Await(&dvrB.IsCurrent, OnRetry(func() {
+				_, _ = TryAct(c, SetWorkerDeploymentCurrentVersion{Version: dvrB})
+			}))
+			wftB.Cancel()
+
+			// Start child workflow
+			childTaskQueue := GenEnum[*model.TaskQueue]("child task queue",
+				tq,                                     // TODO: add labels
+				Act(c, CreateTaskQueue{Namespace: ns}), // TODO: do lazy eval
+			).Next(s)
+			wft := Act(wkr, RespondWorkflowTaskCompleted{
+				Task:               wftA.Await(),
+				Commands:           GenList(StartChildWorkflowExecutionCommand{TaskQueue: childTaskQueue}),
+				Version:            dvrA,
+				VersioningBehavior: AnyVersionBehavior,
+			})
+
+			// Complete child workflow
+			pullChildWorkflowVersion := dvrB
+			if childTaskQueue == tq && wft.Response.VersioningBehavior == enumspb.VERSIONING_BEHAVIOR_PINNED {
+				pullChildWorkflowVersion = dvrA
+			}
+			cwft := Act(wkr, PollWorkflowTaskQueue{TaskQueue: childTaskQueue, Version: pullChildWorkflowVersion})
+			Act(wkr, RespondWorkflowTaskCompleted{
+				Task:     cwft,
+				Commands: GenList(CompleteWorkflowExecutionCommand{}),
+			})
+		})
+	})
+}