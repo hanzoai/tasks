temporalio/temporal

State machine for CHASM activities (#8496)
## What changed?
Adds a state machine defining valid transitions for a CHASM activity.

The state machine does not include transitions related to cancel and
pause states: we can add those when we implement those features.


## Why?
Part of foundations for CHASM activity implementation / Standalone
Activity.

## How did you test it?
- Built (a unit test was written but not committed)

chasm/lib/activity/statemachine.go
@@ -1,0 +1,90 @@
+package activity
+
+import (
+	"go.temporal.io/server/chasm"
+	activitypb "go.temporal.io/server/chasm/lib/activity/gen/activitypb/v1"
+)
+
+// Ensure that Activity implements chasm.StateMachine interface
+var _ chasm.StateMachine[activitypb.ActivityExecutionStatus] = (*Activity)(nil)
+
+// State returns the current status of the activity.
+func (a *Activity) State() activitypb.ActivityExecutionStatus {
+	if a.ActivityState == nil {
+		return activitypb.ACTIVITY_EXECUTION_STATUS_UNSPECIFIED
+	}
+	return a.ActivityState.Status
+}
+
+// SetState sets the status of the activity.
+func (a *Activity) SetState(state activitypb.ActivityExecutionStatus) {
+	a.ActivityState.Status = state
+}
+
+// TransitionScheduled effects a transition to Scheduled status
+var TransitionScheduled = chasm.NewTransition(
+	[]activitypb.ActivityExecutionStatus{
+		activitypb.ACTIVITY_EXECUTION_STATUS_UNSPECIFIED,
+	},
+	activitypb.ACTIVITY_EXECUTION_STATUS_SCHEDULED,
+	func(_ *Activity, _ chasm.MutableContext, _ any) error {
+		return nil
+	},
+)
+
+// TransitionStarted effects a transition to Started status
+var TransitionStarted = chasm.NewTransition(
+	[]activitypb.ActivityExecutionStatus{
+		activitypb.ACTIVITY_EXECUTION_STATUS_SCHEDULED,
+	},
+	activitypb.ACTIVITY_EXECUTION_STATUS_STARTED,
+	func(_ *Activity, _ chasm.MutableContext, _ any) error {
+		return nil
+	},
+)
+
+// TransitionCompleted effects a transition to Completed status
+var TransitionCompleted = chasm.NewTransition(
+	[]activitypb.ActivityExecutionStatus{
+		activitypb.ACTIVITY_EXECUTION_STATUS_STARTED,
+	},
+	activitypb.ACTIVITY_EXECUTION_STATUS_COMPLETED,
+	func(_ *Activity, _ chasm.MutableContext, _ any) error {
+		return nil
+	},
+)
+
+// TransitionFailed effects a transition to Failed status
+var TransitionFailed = chasm.NewTransition(
+	[]activitypb.ActivityExecutionStatus{
+		activitypb.ACTIVITY_EXECUTION_STATUS_STARTED,
+	},
+	activitypb.ACTIVITY_EXECUTION_STATUS_FAILED,
+	func(_ *Activity, _ chasm.MutableContext, _ any) error {
+		return nil
+	},
+)
+
+// TransitionTerminated effects a transition to Terminated status
+var TransitionTerminated = chasm.NewTransition(
+	[]activitypb.ActivityExecutionStatus{
+		activitypb.ACTIVITY_EXECUTION_STATUS_SCHEDULED,
+		activitypb.ACTIVITY_EXECUTION_STATUS_STARTED,
+	},
+	activitypb.ACTIVITY_EXECUTION_STATUS_TERMINATED,
+	func(_ *Activity, _ chasm.MutableContext, _ any) error {
+		return nil
+	},
+)
+
+// TransitionTimedOut effects a transition to TimedOut status
+var TransitionTimedOut = chasm.NewTransition(
+	[]activitypb.ActivityExecutionStatus{
+		activitypb.ACTIVITY_EXECUTION_STATUS_SCHEDULED,
+		activitypb.ACTIVITY_EXECUTION_STATUS_STARTED,
+	},
+	activitypb.ACTIVITY_EXECUTION_STATUS_TIMED_OUT,
+	func(_ *Activity, _ chasm.MutableContext, _ any) error {
+		return nil
+	},
+)

hanzoai/tasks

chasm/lib/activity/statemachine.go
@@ -1,0 +1,90 @@
+package activity
+
+import (
+	"go.temporal.io/server/chasm"
+	activitypb "go.temporal.io/server/chasm/lib/activity/gen/activitypb/v1"
+)
+
+// Ensure that Activity implements chasm.StateMachine interface
+var _ chasm.StateMachine[activitypb.ActivityExecutionStatus] = (*Activity)(nil)
+
+// State returns the current status of the activity.
+func (a *Activity) StateMachineState() activitypb.ActivityExecutionStatus {
+	if a.ActivityState == nil {
+		return activitypb.ACTIVITY_EXECUTION_STATUS_UNSPECIFIED
+	}
+	return a.ActivityState.Status
+}
+
+// SetState sets the status of the activity.
+func (a *Activity) SetStateMachineState(state activitypb.ActivityExecutionStatus) {
+	a.ActivityState.Status = state
+}
+
+// TransitionScheduled effects a transition to Scheduled status
+var TransitionScheduled = chasm.NewTransition(
+	[]activitypb.ActivityExecutionStatus{
+		activitypb.ACTIVITY_EXECUTION_STATUS_UNSPECIFIED,
+	},
+	activitypb.ACTIVITY_EXECUTION_STATUS_SCHEDULED,
+	func(_ *Activity, _ chasm.MutableContext, _ any) error {
+		return nil
+	},
+)
+
+// TransitionStarted effects a transition to Started status
+var TransitionStarted = chasm.NewTransition(
+	[]activitypb.ActivityExecutionStatus{
+		activitypb.ACTIVITY_EXECUTION_STATUS_SCHEDULED,
+	},
+	activitypb.ACTIVITY_EXECUTION_STATUS_STARTED,
+	func(_ *Activity, _ chasm.MutableContext, _ any) error {
+		return nil
+	},
+)
+
+// TransitionCompleted effects a transition to Completed status
+var TransitionCompleted = chasm.NewTransition(
+	[]activitypb.ActivityExecutionStatus{
+		activitypb.ACTIVITY_EXECUTION_STATUS_STARTED,
+	},
+	activitypb.ACTIVITY_EXECUTION_STATUS_COMPLETED,
+	func(_ *Activity, _ chasm.MutableContext, _ any) error {
+		return nil
+	},
+)
+
+// TransitionFailed effects a transition to Failed status
+var TransitionFailed = chasm.NewTransition(
+	[]activitypb.ActivityExecutionStatus{
+		activitypb.ACTIVITY_EXECUTION_STATUS_STARTED,
+	},
+	activitypb.ACTIVITY_EXECUTION_STATUS_FAILED,
+	func(_ *Activity, _ chasm.MutableContext, _ any) error {
+		return nil
+	},
+)
+
+// TransitionTerminated effects a transition to Terminated status
+var TransitionTerminated = chasm.NewTransition(
+	[]activitypb.ActivityExecutionStatus{
+		activitypb.ACTIVITY_EXECUTION_STATUS_SCHEDULED,
+		activitypb.ACTIVITY_EXECUTION_STATUS_STARTED,
+	},
+	activitypb.ACTIVITY_EXECUTION_STATUS_TERMINATED,
+	func(_ *Activity, _ chasm.MutableContext, _ any) error {
+		return nil
+	},
+)
+
+// TransitionTimedOut effects a transition to TimedOut status
+var TransitionTimedOut = chasm.NewTransition(
+	[]activitypb.ActivityExecutionStatus{
+		activitypb.ACTIVITY_EXECUTION_STATUS_SCHEDULED,
+		activitypb.ACTIVITY_EXECUTION_STATUS_STARTED,
+	},
+	activitypb.ACTIVITY_EXECUTION_STATUS_TIMED_OUT,
+	func(_ *Activity, _ chasm.MutableContext, _ any) error {
+		return nil
+	},
+)