dandavison/temporalio-temporal

CHASM stream boilerplate

chasm/lib/stream/gen/streampb/v1/request_response.go-helpers.pb.go
@@ -1,0 +1,154 @@
+// Code generated by protoc-gen-go-helpers. DO NOT EDIT.
+package streampb
+
+import (
+	"google.golang.org/protobuf/proto"
+)
+
+// Marshal an object of type PushStreamRequest to the protobuf v3 wire format
+func (val *PushStreamRequest) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type PushStreamRequest from the protobuf v3 wire format
+func (val *PushStreamRequest) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *PushStreamRequest) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two PushStreamRequest values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *PushStreamRequest) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *PushStreamRequest
+	switch t := that.(type) {
+	case *PushStreamRequest:
+		that1 = t
+	case PushStreamRequest:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}
+
+// Marshal an object of type PushStreamResponse to the protobuf v3 wire format
+func (val *PushStreamResponse) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type PushStreamResponse from the protobuf v3 wire format
+func (val *PushStreamResponse) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *PushStreamResponse) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two PushStreamResponse values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *PushStreamResponse) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *PushStreamResponse
+	switch t := that.(type) {
+	case *PushStreamResponse:
+		that1 = t
+	case PushStreamResponse:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}
+
+// Marshal an object of type PollStreamRequest to the protobuf v3 wire format
+func (val *PollStreamRequest) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type PollStreamRequest from the protobuf v3 wire format
+func (val *PollStreamRequest) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *PollStreamRequest) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two PollStreamRequest values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *PollStreamRequest) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *PollStreamRequest
+	switch t := that.(type) {
+	case *PollStreamRequest:
+		that1 = t
+	case PollStreamRequest:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}
+
+// Marshal an object of type PollStreamResponse to the protobuf v3 wire format
+func (val *PollStreamResponse) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type PollStreamResponse from the protobuf v3 wire format
+func (val *PollStreamResponse) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *PollStreamResponse) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two PollStreamResponse values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *PollStreamResponse) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *PollStreamResponse
+	switch t := that.(type) {
+	case *PollStreamResponse:
+		that1 = t
+	case PollStreamResponse:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}

chasm/lib/stream/gen/streampb/v1/service.pb.go
@@ -1,0 +1,76 @@
+// Code generated by protoc-gen-go. DO NOT EDIT.
+// plugins:
+// 	protoc-gen-go
+// 	protoc
+// source: temporal/server/chasm/lib/stream/proto/v1/service.proto
+
+package streampb
+
+import (
+	reflect "reflect"
+	unsafe "unsafe"
+
+	_ "go.temporal.io/server/api/routing/v1"
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+)
+
+const (
+	// Verify that this generated code is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(20 - protoimpl.MinVersion)
+	// Verify that runtime/protoimpl is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
+)
+
+var File_temporal_server_chasm_lib_stream_proto_v1_service_proto protoreflect.FileDescriptor
+
+const file_temporal_server_chasm_lib_stream_proto_v1_service_proto_rawDesc = "" +
+	"\n" +
+	"7temporal/server/chasm/lib/stream/proto/v1/service.proto\x12)temporal.server.chasm.lib.stream.proto.v1\x1a@temporal/server/chasm/lib/stream/proto/v1/request_response.proto\x1a.temporal/server/api/routing/v1/extension.proto2\xeb\x02\n" +
+	"\rStreamService\x12\xab\x01\n" +
+	"\n" +
+	"PushStream\x12<.temporal.server.chasm.lib.stream.proto.v1.PushStreamRequest\x1a=.temporal.server.chasm.lib.stream.proto.v1.PushStreamResponse\" \x92\xc4\x03\x1c\x1a\x1afrontend_request.stream_id\x12\xab\x01\n" +
+	"\n" +
+	"PollStream\x12<.temporal.server.chasm.lib.stream.proto.v1.PollStreamRequest\x1a=.temporal.server.chasm.lib.stream.proto.v1.PollStreamResponse\" \x92\xc4\x03\x1c\x1a\x1afrontend_request.stream_idB>Z<go.temporal.io/server/chasm/lib/stream/gen/streampb;streampbb\x06proto3"
+
+var file_temporal_server_chasm_lib_stream_proto_v1_service_proto_goTypes = []any{
+	(*PushStreamRequest)(nil),  // 0: temporal.server.chasm.lib.stream.proto.v1.PushStreamRequest
+	(*PollStreamRequest)(nil),  // 1: temporal.server.chasm.lib.stream.proto.v1.PollStreamRequest
+	(*PushStreamResponse)(nil), // 2: temporal.server.chasm.lib.stream.proto.v1.PushStreamResponse
+	(*PollStreamResponse)(nil), // 3: temporal.server.chasm.lib.stream.proto.v1.PollStreamResponse
+}
+var file_temporal_server_chasm_lib_stream_proto_v1_service_proto_depIdxs = []int32{
+	0, // 0: temporal.server.chasm.lib.stream.proto.v1.StreamService.PushStream:input_type -> temporal.server.chasm.lib.stream.proto.v1.PushStreamRequest
+	1, // 1: temporal.server.chasm.lib.stream.proto.v1.StreamService.PollStream:input_type -> temporal.server.chasm.lib.stream.proto.v1.PollStreamRequest
+	2, // 2: temporal.server.chasm.lib.stream.proto.v1.StreamService.PushStream:output_type -> temporal.server.chasm.lib.stream.proto.v1.PushStreamResponse
+	3, // 3: temporal.server.chasm.lib.stream.proto.v1.StreamService.PollStream:output_type -> temporal.server.chasm.lib.stream.proto.v1.PollStreamResponse
+	2, // [2:4] is the sub-list for method output_type
+	0, // [0:2] is the sub-list for method input_type
+	0, // [0:0] is the sub-list for extension type_name
+	0, // [0:0] is the sub-list for extension extendee
+	0, // [0:0] is the sub-list for field type_name
+}
+
+func init() { file_temporal_server_chasm_lib_stream_proto_v1_service_proto_init() }
+func file_temporal_server_chasm_lib_stream_proto_v1_service_proto_init() {
+	if File_temporal_server_chasm_lib_stream_proto_v1_service_proto != nil {
+		return
+	}
+	file_temporal_server_chasm_lib_stream_proto_v1_request_response_proto_init()
+	type x struct{}
+	out := protoimpl.TypeBuilder{
+		File: protoimpl.DescBuilder{
+			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
+			RawDescriptor: unsafe.Slice(unsafe.StringData(file_temporal_server_chasm_lib_stream_proto_v1_service_proto_rawDesc), len(file_temporal_server_chasm_lib_stream_proto_v1_service_proto_rawDesc)),
+			NumEnums:      0,
+			NumMessages:   0,
+			NumExtensions: 0,
+			NumServices:   1,
+		},
+		GoTypes:           file_temporal_server_chasm_lib_stream_proto_v1_service_proto_goTypes,
+		DependencyIndexes: file_temporal_server_chasm_lib_stream_proto_v1_service_proto_depIdxs,
+	}.Build()
+	File_temporal_server_chasm_lib_stream_proto_v1_service_proto = out.File
+	file_temporal_server_chasm_lib_stream_proto_v1_service_proto_goTypes = nil
+	file_temporal_server_chasm_lib_stream_proto_v1_service_proto_depIdxs = nil
+}

client/frontend/metric_client_gen.go
@@ -765,6 +765,20 @@ func (c *metricClient) PollNexusTaskQueue(
 	return c.client.PollNexusTaskQueue(ctx, request, opts...)
 }
 
+func (c *metricClient) PollStream(
+	ctx context.Context,
+	request *workflowservice.PollStreamRequest,
+	opts ...grpc.CallOption,
+) (_ *workflowservice.PollStreamResponse, retError error) {
+
+	metricsHandler, startTime := c.startMetricsRecording(ctx, "FrontendClientPollStream")
+	defer func() {
+		c.finishMetricsRecording(metricsHandler, startTime, retError)
+	}()
+
+	return c.client.PollStream(ctx, request, opts...)
+}
+
 func (c *metricClient) PollWorkflowExecutionUpdate(
 	ctx context.Context,
 	request *workflowservice.PollWorkflowExecutionUpdateRequest,
@@ -791,6 +805,20 @@ func (c *metricClient) PollWorkflowTaskQueue(
 	}()
 
 	return c.client.PollWorkflowTaskQueue(ctx, request, opts...)
+}
+
+func (c *metricClient) PushStream(
+	ctx context.Context,
+	request *workflowservice.PushStreamRequest,
+	opts ...grpc.CallOption,
+) (_ *workflowservice.PushStreamResponse, retError error) {
+
+	metricsHandler, startTime := c.startMetricsRecording(ctx, "FrontendClientPushStream")
+	defer func() {
+		c.finishMetricsRecording(metricsHandler, startTime, retError)
+	}()
+
+	return c.client.PushStream(ctx, request, opts...)
 }
 
 func (c *metricClient) QueryWorkflow(

client/frontend/retryable_client_gen.go
@@ -821,6 +821,21 @@ func (c *retryableClient) PollNexusTaskQueue(
 	return resp, err
 }
 
+func (c *retryableClient) PollStream(
+	ctx context.Context,
+	request *workflowservice.PollStreamRequest,
+	opts ...grpc.CallOption,
+) (*workflowservice.PollStreamResponse, error) {
+	var resp *workflowservice.PollStreamResponse
+	op := func(ctx context.Context) error {
+		var err error
+		resp, err = c.client.PollStream(ctx, request, opts...)
+		return err
+	}
+	err := backoff.ThrottleRetryContext(ctx, op, c.policy, c.isRetryable)
+	return resp, err
+}
+
 func (c *retryableClient) PollWorkflowExecutionUpdate(
 	ctx context.Context,
 	request *workflowservice.PollWorkflowExecutionUpdateRequest,
@@ -845,6 +860,21 @@ func (c *retryableClient) PollWorkflowTaskQueue(
 	op := func(ctx context.Context) error {
 		var err error
 		resp, err = c.client.PollWorkflowTaskQueue(ctx, request, opts...)
+		return err
+	}
+	err := backoff.ThrottleRetryContext(ctx, op, c.policy, c.isRetryable)
+	return resp, err
+}
+
+func (c *retryableClient) PushStream(
+	ctx context.Context,
+	request *workflowservice.PushStreamRequest,
+	opts ...grpc.CallOption,
+) (*workflowservice.PushStreamResponse, error) {
+	var resp *workflowservice.PushStreamResponse
+	op := func(ctx context.Context) error {
+		var err error
+		resp, err = c.client.PushStream(ctx, request, opts...)
 		return err
 	}
 	err := backoff.ThrottleRetryContext(ctx, op, c.policy, c.isRetryable)

common/testing/mockapi/workflowservicemock/v1/service_grpc.pb.mock.go
@@ -1122,6 +1122,26 @@ func (mr *MockWorkflowServiceClientMockRecorder) PollNexusTaskQueue(ctx, in any,
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "PollNexusTaskQueue", reflect.TypeOf((*MockWorkflowServiceClient)(nil).PollNexusTaskQueue), varargs...)
 }
 
+// PollStream mocks base method.
+func (m *MockWorkflowServiceClient) PollStream(ctx context.Context, in *workflowservice.PollStreamRequest, opts ...grpc.CallOption) (*workflowservice.PollStreamResponse, error) {
+	m.ctrl.T.Helper()
+	varargs := []any{ctx, in}
+	for _, a := range opts {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "PollStream", varargs...)
+	ret0, _ := ret[0].(*workflowservice.PollStreamResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// PollStream indicates an expected call of PollStream.
+func (mr *MockWorkflowServiceClientMockRecorder) PollStream(ctx, in any, opts ...any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]any{ctx, in}, opts...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "PollStream", reflect.TypeOf((*MockWorkflowServiceClient)(nil).PollStream), varargs...)
+}
+
 // PollWorkflowExecutionUpdate mocks base method.
 func (m *MockWorkflowServiceClient) PollWorkflowExecutionUpdate(ctx context.Context, in *workflowservice.PollWorkflowExecutionUpdateRequest, opts ...grpc.CallOption) (*workflowservice.PollWorkflowExecutionUpdateResponse, error) {
 	m.ctrl.T.Helper()
@@ -1160,6 +1180,26 @@ func (mr *MockWorkflowServiceClientMockRecorder) PollWorkflowTaskQueue(ctx, in a
 	mr.mock.ctrl.T.Helper()
 	varargs := append([]any{ctx, in}, opts...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "PollWorkflowTaskQueue", reflect.TypeOf((*MockWorkflowServiceClient)(nil).PollWorkflowTaskQueue), varargs...)
+}
+
+// PushStream mocks base method.
+func (m *MockWorkflowServiceClient) PushStream(ctx context.Context, in *workflowservice.PushStreamRequest, opts ...grpc.CallOption) (*workflowservice.PushStreamResponse, error) {
+	m.ctrl.T.Helper()
+	varargs := []any{ctx, in}
+	for _, a := range opts {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "PushStream", varargs...)
+	ret0, _ := ret[0].(*workflowservice.PushStreamResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// PushStream indicates an expected call of PushStream.
+func (mr *MockWorkflowServiceClientMockRecorder) PushStream(ctx, in any, opts ...any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]any{ctx, in}, opts...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "PushStream", reflect.TypeOf((*MockWorkflowServiceClient)(nil).PushStream), varargs...)
 }
 
 // QueryWorkflow mocks base method.

hanzoai/tasks

chasm/lib/stream/gen/streampb/v1/request_response.go-helpers.pb.go
@@ -1,0 +1,154 @@
+// Code generated by protoc-gen-go-helpers. DO NOT EDIT.
+package streampb
+
+import (
+	"google.golang.org/protobuf/proto"
+)
+
+// Marshal an object of type AddToStreamRequest to the protobuf v3 wire format
+func (val *AddToStreamRequest) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type AddToStreamRequest from the protobuf v3 wire format
+func (val *AddToStreamRequest) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *AddToStreamRequest) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two AddToStreamRequest values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *AddToStreamRequest) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *AddToStreamRequest
+	switch t := that.(type) {
+	case *AddToStreamRequest:
+		that1 = t
+	case AddToStreamRequest:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}
+
+// Marshal an object of type AddToStreamResponse to the protobuf v3 wire format
+func (val *AddToStreamResponse) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type AddToStreamResponse from the protobuf v3 wire format
+func (val *AddToStreamResponse) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *AddToStreamResponse) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two AddToStreamResponse values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *AddToStreamResponse) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *AddToStreamResponse
+	switch t := that.(type) {
+	case *AddToStreamResponse:
+		that1 = t
+	case AddToStreamResponse:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}
+
+// Marshal an object of type PollStreamRequest to the protobuf v3 wire format
+func (val *PollStreamRequest) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type PollStreamRequest from the protobuf v3 wire format
+func (val *PollStreamRequest) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *PollStreamRequest) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two PollStreamRequest values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *PollStreamRequest) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *PollStreamRequest
+	switch t := that.(type) {
+	case *PollStreamRequest:
+		that1 = t
+	case PollStreamRequest:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}
+
+// Marshal an object of type PollStreamResponse to the protobuf v3 wire format
+func (val *PollStreamResponse) Marshal() ([]byte, error) {
+	return proto.Marshal(val)
+}
+
+// Unmarshal an object of type PollStreamResponse from the protobuf v3 wire format
+func (val *PollStreamResponse) Unmarshal(buf []byte) error {
+	return proto.Unmarshal(buf, val)
+}
+
+// Size returns the size of the object, in bytes, once serialized
+func (val *PollStreamResponse) Size() int {
+	return proto.Size(val)
+}
+
+// Equal returns whether two PollStreamResponse values are equivalent by recursively
+// comparing the message's fields.
+// For more information see the documentation for
+// https://pkg.go.dev/google.golang.org/protobuf/proto#Equal
+func (this *PollStreamResponse) Equal(that interface{}) bool {
+	if that == nil {
+		return this == nil
+	}
+
+	var that1 *PollStreamResponse
+	switch t := that.(type) {
+	case *PollStreamResponse:
+		that1 = t
+	case PollStreamResponse:
+		that1 = &t
+	default:
+		return false
+	}
+
+	return proto.Equal(this, that1)
+}

chasm/lib/stream/gen/streampb/v1/service.pb.go
@@ -1,0 +1,75 @@
+// Code generated by protoc-gen-go. DO NOT EDIT.
+// plugins:
+// 	protoc-gen-go
+// 	protoc
+// source: temporal/server/chasm/lib/stream/proto/v1/service.proto
+
+package streampb
+
+import (
+	reflect "reflect"
+	unsafe "unsafe"
+
+	_ "go.temporal.io/server/api/routing/v1"
+	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
+	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+)
+
+const (
+	// Verify that this generated code is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(20 - protoimpl.MinVersion)
+	// Verify that runtime/protoimpl is sufficiently up-to-date.
+	_ = protoimpl.EnforceVersion(protoimpl.MaxVersion - 20)
+)
+
+var File_temporal_server_chasm_lib_stream_proto_v1_service_proto protoreflect.FileDescriptor
+
+const file_temporal_server_chasm_lib_stream_proto_v1_service_proto_rawDesc = "" +
+	"\n" +
+	"7temporal/server/chasm/lib/stream/proto/v1/service.proto\x12)temporal.server.chasm.lib.stream.proto.v1\x1a@temporal/server/chasm/lib/stream/proto/v1/request_response.proto\x1a.temporal/server/api/routing/v1/extension.proto2\xee\x02\n" +
+	"\rStreamService\x12\xae\x01\n" +
+	"\vAddToStream\x12=.temporal.server.chasm.lib.stream.proto.v1.AddToStreamRequest\x1a>.temporal.server.chasm.lib.stream.proto.v1.AddToStreamResponse\" \x92\xc4\x03\x1c\x1a\x1afrontend_request.stream_id\x12\xab\x01\n" +
+	"\n" +
+	"PollStream\x12<.temporal.server.chasm.lib.stream.proto.v1.PollStreamRequest\x1a=.temporal.server.chasm.lib.stream.proto.v1.PollStreamResponse\" \x92\xc4\x03\x1c\x1a\x1afrontend_request.stream_idB>Z<go.temporal.io/server/chasm/lib/stream/gen/streampb;streampbb\x06proto3"
+
+var file_temporal_server_chasm_lib_stream_proto_v1_service_proto_goTypes = []any{
+	(*AddToStreamRequest)(nil),  // 0: temporal.server.chasm.lib.stream.proto.v1.AddToStreamRequest
+	(*PollStreamRequest)(nil),   // 1: temporal.server.chasm.lib.stream.proto.v1.PollStreamRequest
+	(*AddToStreamResponse)(nil), // 2: temporal.server.chasm.lib.stream.proto.v1.AddToStreamResponse
+	(*PollStreamResponse)(nil),  // 3: temporal.server.chasm.lib.stream.proto.v1.PollStreamResponse
+}
+var file_temporal_server_chasm_lib_stream_proto_v1_service_proto_depIdxs = []int32{
+	0, // 0: temporal.server.chasm.lib.stream.proto.v1.StreamService.AddToStream:input_type -> temporal.server.chasm.lib.stream.proto.v1.AddToStreamRequest
+	1, // 1: temporal.server.chasm.lib.stream.proto.v1.StreamService.PollStream:input_type -> temporal.server.chasm.lib.stream.proto.v1.PollStreamRequest
+	2, // 2: temporal.server.chasm.lib.stream.proto.v1.StreamService.AddToStream:output_type -> temporal.server.chasm.lib.stream.proto.v1.AddToStreamResponse
+	3, // 3: temporal.server.chasm.lib.stream.proto.v1.StreamService.PollStream:output_type -> temporal.server.chasm.lib.stream.proto.v1.PollStreamResponse
+	2, // [2:4] is the sub-list for method output_type
+	0, // [0:2] is the sub-list for method input_type
+	0, // [0:0] is the sub-list for extension type_name
+	0, // [0:0] is the sub-list for extension extendee
+	0, // [0:0] is the sub-list for field type_name
+}
+
+func init() { file_temporal_server_chasm_lib_stream_proto_v1_service_proto_init() }
+func file_temporal_server_chasm_lib_stream_proto_v1_service_proto_init() {
+	if File_temporal_server_chasm_lib_stream_proto_v1_service_proto != nil {
+		return
+	}
+	file_temporal_server_chasm_lib_stream_proto_v1_request_response_proto_init()
+	type x struct{}
+	out := protoimpl.TypeBuilder{
+		File: protoimpl.DescBuilder{
+			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
+			RawDescriptor: unsafe.Slice(unsafe.StringData(file_temporal_server_chasm_lib_stream_proto_v1_service_proto_rawDesc), len(file_temporal_server_chasm_lib_stream_proto_v1_service_proto_rawDesc)),
+			NumEnums:      0,
+			NumMessages:   0,
+			NumExtensions: 0,
+			NumServices:   1,
+		},
+		GoTypes:           file_temporal_server_chasm_lib_stream_proto_v1_service_proto_goTypes,
+		DependencyIndexes: file_temporal_server_chasm_lib_stream_proto_v1_service_proto_depIdxs,
+	}.Build()
+	File_temporal_server_chasm_lib_stream_proto_v1_service_proto = out.File
+	file_temporal_server_chasm_lib_stream_proto_v1_service_proto_goTypes = nil
+	file_temporal_server_chasm_lib_stream_proto_v1_service_proto_depIdxs = nil
+}

client/frontend/metric_client_gen.go
@@ -9,6 +9,20 @@ import (
 	"google.golang.org/grpc"
 )
 
+func (c *metricClient) AddToStream(
+	ctx context.Context,
+	request *workflowservice.AddToStreamRequest,
+	opts ...grpc.CallOption,
+) (_ *workflowservice.AddToStreamResponse, retError error) {
+
+	metricsHandler, startTime := c.startMetricsRecording(ctx, "FrontendClientAddToStream")
+	defer func() {
+		c.finishMetricsRecording(metricsHandler, startTime, retError)
+	}()
+
+	return c.client.AddToStream(ctx, request, opts...)
+}
+
 func (c *metricClient) CountActivityExecutions(
 	ctx context.Context,
 	request *workflowservice.CountActivityExecutionsRequest,
@@ -763,6 +777,20 @@ func (c *metricClient) PollNexusTaskQueue(
 	}()
 
 	return c.client.PollNexusTaskQueue(ctx, request, opts...)
+}
+
+func (c *metricClient) PollStream(
+	ctx context.Context,
+	request *workflowservice.PollStreamRequest,
+	opts ...grpc.CallOption,
+) (_ *workflowservice.PollStreamResponse, retError error) {
+
+	metricsHandler, startTime := c.startMetricsRecording(ctx, "FrontendClientPollStream")
+	defer func() {
+		c.finishMetricsRecording(metricsHandler, startTime, retError)
+	}()
+
+	return c.client.PollStream(ctx, request, opts...)
 }
 
 func (c *metricClient) PollWorkflowExecutionUpdate(

client/frontend/retryable_client_gen.go
@@ -11,6 +11,21 @@ import (
 	"go.temporal.io/server/common/backoff"
 )
 
+func (c *retryableClient) AddToStream(
+	ctx context.Context,
+	request *workflowservice.AddToStreamRequest,
+	opts ...grpc.CallOption,
+) (*workflowservice.AddToStreamResponse, error) {
+	var resp *workflowservice.AddToStreamResponse
+	op := func(ctx context.Context) error {
+		var err error
+		resp, err = c.client.AddToStream(ctx, request, opts...)
+		return err
+	}
+	err := backoff.ThrottleRetryContext(ctx, op, c.policy, c.isRetryable)
+	return resp, err
+}
+
 func (c *retryableClient) CountActivityExecutions(
 	ctx context.Context,
 	request *workflowservice.CountActivityExecutionsRequest,
@@ -815,6 +830,21 @@ func (c *retryableClient) PollNexusTaskQueue(
 	op := func(ctx context.Context) error {
 		var err error
 		resp, err = c.client.PollNexusTaskQueue(ctx, request, opts...)
+		return err
+	}
+	err := backoff.ThrottleRetryContext(ctx, op, c.policy, c.isRetryable)
+	return resp, err
+}
+
+func (c *retryableClient) PollStream(
+	ctx context.Context,
+	request *workflowservice.PollStreamRequest,
+	opts ...grpc.CallOption,
+) (*workflowservice.PollStreamResponse, error) {
+	var resp *workflowservice.PollStreamResponse
+	op := func(ctx context.Context) error {
+		var err error
+		resp, err = c.client.PollStream(ctx, request, opts...)
 		return err
 	}
 	err := backoff.ThrottleRetryContext(ctx, op, c.policy, c.isRetryable)

common/testing/mockapi/workflowservicemock/v1/service_grpc.pb.mock.go
@@ -42,6 +42,26 @@ func (m *MockWorkflowServiceClient) EXPECT() *MockWorkflowServiceClientMockRecor
 	return m.recorder
 }
 
+// AddToStream mocks base method.
+func (m *MockWorkflowServiceClient) AddToStream(ctx context.Context, in *workflowservice.AddToStreamRequest, opts ...grpc.CallOption) (*workflowservice.AddToStreamResponse, error) {
+	m.ctrl.T.Helper()
+	varargs := []any{ctx, in}
+	for _, a := range opts {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "AddToStream", varargs...)
+	ret0, _ := ret[0].(*workflowservice.AddToStreamResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// AddToStream indicates an expected call of AddToStream.
+func (mr *MockWorkflowServiceClientMockRecorder) AddToStream(ctx, in any, opts ...any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]any{ctx, in}, opts...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "AddToStream", reflect.TypeOf((*MockWorkflowServiceClient)(nil).AddToStream), varargs...)
+}
+
 // CountActivityExecutions mocks base method.
 func (m *MockWorkflowServiceClient) CountActivityExecutions(ctx context.Context, in *workflowservice.CountActivityExecutionsRequest, opts ...grpc.CallOption) (*workflowservice.CountActivityExecutionsResponse, error) {
 	m.ctrl.T.Helper()
@@ -1120,6 +1140,26 @@ func (mr *MockWorkflowServiceClientMockRecorder) PollNexusTaskQueue(ctx, in any,
 	mr.mock.ctrl.T.Helper()
 	varargs := append([]any{ctx, in}, opts...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "PollNexusTaskQueue", reflect.TypeOf((*MockWorkflowServiceClient)(nil).PollNexusTaskQueue), varargs...)
+}
+
+// PollStream mocks base method.
+func (m *MockWorkflowServiceClient) PollStream(ctx context.Context, in *workflowservice.PollStreamRequest, opts ...grpc.CallOption) (*workflowservice.PollStreamResponse, error) {
+	m.ctrl.T.Helper()
+	varargs := []any{ctx, in}
+	for _, a := range opts {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "PollStream", varargs...)
+	ret0, _ := ret[0].(*workflowservice.PollStreamResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// PollStream indicates an expected call of PollStream.
+func (mr *MockWorkflowServiceClientMockRecorder) PollStream(ctx, in any, opts ...any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]any{ctx, in}, opts...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "PollStream", reflect.TypeOf((*MockWorkflowServiceClient)(nil).PollStream), varargs...)
 }
 
 // PollWorkflowExecutionUpdate mocks base method.